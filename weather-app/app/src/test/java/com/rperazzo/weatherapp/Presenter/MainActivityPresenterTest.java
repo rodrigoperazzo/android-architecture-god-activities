fc0ceb6af9f44b792c4c4d54b9aee9a1b4e235c9
aluno <aluno@LABMAC17.local>
2018-11-10 18:02:22 -0300

    end commit

--- weather-app/app/src/test/java/com/rperazzo/weatherapp/Presenter/MainActivityPresenterTest.java
+++ weather-app/app/src/test/java/com/rperazzo/weatherapp/Presenter/MainActivityPresenterTest.java
@@ -11,49 +11,27 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
 
-public class MainActivityPresenterTest implements  IView  {
+public class MainActivityPresenterTest   {
 
+    IView _view;
     MainActivityPresenter _presenter;
     Context _context;
     @Before
     public void before() throws Exception {
+        _view = Mockito.mock(IView.class);
         _context = Mockito.mock(Context.class);
-
     }
-
 
     @Test
     public void searchByNameOlindaExpectedRecordOlinda() {
-        _presenter = new MainActivityPresenter(this);
+        _presenter = new MainActivityPresenter(_view);
         _presenter.searchByName("Olinda");
-
-
-    }
-
-    @Override
-    public void onFinishLoading(WeatherManager.FindResult result) {
-    }
-
-    @Override
-    public void onFinishLoadingWithError() {
-
-    }
-
-    @Override
-    public Context getContext() {
-        return _context;
-    }
-
-    @Override
-    public void onStartLoading() {
-
-    }
-
-    @Override
-    public void showMessage(String message) {
+        Mockito.verify(_view,Mockito.never()).onFinishLoadingWithError();
 
     }
 }
